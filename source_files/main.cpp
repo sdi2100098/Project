8fbc883cb1d65320c6354fbfa75c006f22b16681
sdi2100098 <sdi2100098@di.uoa.gr>
2024-10-11 21:25:00 +0300

    Test

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -4,11 +4,15 @@
 int main(void){
 
     // Example usage:
+    std::vector<std::vector<float>> vectors;
+    std::vector<float> myvector;
     std::string input_filename = "Datasets/siftsmall_base.fvecs";
     std::string output_filename = "siftsmall_base.txt";
     
     // Read vectors and write to text file
-    fvecs_read(input_filename, output_filename, {1, -1});  // Read all vectors
+    vectors = fvecs_read(input_filename, output_filename, {1, -1});  // Read all vectors
+    myvector = vectors.front();
     std::cout << "Vectors have been written to " << output_filename << std::endl;
+    std::cout << myvector.front() << std::endl;
     return 0;
 }
