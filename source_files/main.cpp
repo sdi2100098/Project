ce3b40b279581eb1d1e407833d9c41d41c304e4a
sdi2100098 <sdi2100098@di.uoa.gr>
2024-10-14 14:35:26 +0300

    Changes in Main

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -1,8 +1,8 @@
 #include "Library.hpp"
 
-int main(void){
+int main(){
 
-    const char *input_filename = "/root/Project/Project1/Project-1/Datasets/siftsmall_base.fvecs";
+    const char *input_filename = "Datasets/siftsmall_base.fvecs";
     int count = 0;
     float **vectors_base ;
     float **vectors_query;
@@ -21,24 +21,5 @@ int main(void){
             printf("%.4f ",vectors_base[i][j]);
         }
     }
-
-
-    // Read the vectors using the fvecs_read function
-    // while (count < 2) {
-    //     //std::cout << "Give path for file : " ; // Here the path is given from the user
-    //     //std::cin >> input_filename;
-    //     //scanf("%s",input_filename);
-    //     if(!count)
-    //         vectors_base = Read_Data(input_filename, &num_vectors[count], &vector_dim[count]);
-    //     else
-    //         vectors_query = Read_Data(input_filename, &num_vectors[count], &vector_dim[count]);
-    //     if(!vectors_base || !vectors_query){ // In case the pointer is null print error message and retake the path
-    //         return 1;
-    //     }
-    //     count ++;
-    // }
-
-
-    //std::cout << vectors_base[0][0] << std::endl << vectors_query[0][0]<<std::endl ;
     return 0;
 }
