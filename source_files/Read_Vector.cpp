6c906467dd88de0c386bf294543e2f53bc54ed49
sdi2100028 <sdi2100028@di.uoa.gr>
2024-10-17 14:12:45 +0300

    C1

--- source_files/Read_Vector.cpp
+++ source_files/Read_Vector.cpp
@@ -51,7 +51,7 @@ int Init_Graph_Data(const char *file_path, Graph *graph){
 
     for(int index = 0; index < graph->number_of_nodes; index++){
         while(graph->nodes_array[index].edges.size() < graph->R){ // while the size of set is smaller than R
-            random_number = GetRandomNumber(0,graph->number_of_nodes,index); // min = 0, max = number of Vectors, number to exclude is the index of the current node
+            random_number = GetRandomNumber(graph->number_of_nodes,index); // min = 0, max = number of Vectors, number to exclude is the index of the current node
             try{
                 graph->nodes_array[index].edges.insert(random_number); // insert the element
             }catch(const std::bad_alloc& error){goto memmory_error;}
