969ca9f9e245b7ea1212ad26d79a7d776d977f6f
GianKouvelas <gianniskouv13@gmail.com>
2024-10-17 22:25:17 +0300

    Hash function-1

--- source_files/Hash.cpp
+++ source_files/Hash.cpp
@@ -2,11 +2,11 @@
 #define M (1ULL << 32) //Define a large number
 #define P 31 //Define a prime number
 
-int Hash(Query *query,int vector) {
+int Hash(const float *vector_array,int dimension) {
     int hash = 0; //Initialize the hash 
-    for(int i = query->dimension;i = 1;i--) {
-        double d = (double)query->vectors_array[vector][query->dimension - i];//Covert the float to double for more accuracy
-        hash = hash + (d * pow(P,query->dimension - i));
+    for(int i = 0;i <= dimension-1;i--) {
+        double d = (double)vector_array[i];//Covert the float to double for more accuracy
+        hash = hash + (d * pow(P,i));
     }
     hash = floor(hash);
     hash = hash % M;
