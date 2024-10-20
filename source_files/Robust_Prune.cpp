0d2b1feeea4098a4aabf61ba67d1aca7f0afa010
sdi2100098 <sdi2100098@di.uoa.gr>
2024-10-20 23:02:42 +0300

    Branches are so difficult to understand

--- source_files/Robust_Prune.cpp
+++ source_files/Robust_Prune.cpp
@@ -1,10 +1,43 @@
 #include "Library.hpp"
 
-// void Robust_Prune(int p, std::set<int> *V, float a, Graph *G){
-//     return;
-// }
 
-void Test(std::set<int> *x){
-    x->erase(10);
-    //x.erase(10);
+void Robust_Prune(int p, std::set<int> *V, float a, Graph *G){
+
+    int p_star, p_tonos;
+    double min_distance, temp_distance;
+
+    /* V <- (V U Nout(p)) \ {p} */
+    for(std::set<int>::iterator it = G->nodes_array[p].edges.begin(); it != G->nodes_array[p].edges.end(); it++)
+        V->insert(*it);
+    V->erase(p);
+
+    /* Nout(p) <- 0 */
+    G->nodes_array[p].edges.clear();
+
+    while(V->size() != 0){ /* V != 0 */
+    
+        p_star = *(V->begin());
+        min_distance = EuclidianDistance(G->nodes_array[p].vector,G->nodes_array[p_star].vector,G->dimension);
+
+        /* p* <- arg min[for p'in V compute d(p,p')] */
+        for(std::set<int>::iterator it = ++(V->begin()); it != V->end(); it++){
+            temp_distance = EuclidianDistance(G->nodes_array[p].vector,G->nodes_array[*it].vector,G->dimension);
+            if(temp_distance < min_distance){
+                min_distance = temp_distance;
+                p_star = *it;
+            }
+        }
+
+        G->nodes_array[p].edges.insert(p_star); /* Nout(p) <- Nout(p) U {p*} */
+
+        if((int)G->nodes_array[p].edges.size() == G->R) break; /* |Nout(p)| = R */
+        
+        for(std::set<int>::iterator it = V->begin(); it != V->end(); it++){
+            if((a * EuclidianDistance(G->nodes_array[p_star].vector,G->nodes_array[*it].vector,G->dimension))
+            <= EuclidianDistance(G->nodes_array[p].vector,G->nodes_array[*it].vector,G->dimension)){
+                V->erase(*it);
+            }
+        }
+    }
+    return;
 }
