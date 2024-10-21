364a82ca7525c00322b6644520883d9b8dc2aa38
sdi2100098 <sdi2100098@di.uoa.gr>
2024-10-21 18:35:06 +0300

    Testing something with queries

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -6,7 +6,7 @@ int main()
     const char *base_filename = "Datasets/siftsmall_base.fvecs";
     const char *query_filename = "Datasets/siftsmall_query.fvecs";
     int count = 0;
-    int return_number, fun_result = -1;
+    int return_number, fun_result = -1, fun_result_query, check = 0;
     Graph G;
 
     // give the R VERY IMPORTANT
@@ -14,35 +14,49 @@ int main()
 
     Query Q;
 
-    // fun_result = Init_Query_Data(query_filename,&Q);
+    // fun_result_query = Init_Query_Data(query_filename, &Q);
 
     // printf("%d\n",fun_result);
 
     // if(fun_result == 1) return 1;
     fun_result = Init_Graph_Data(base_filename, &G);
+
+    // for (int j = 0; j < G.number_of_nodes; j++)
+    // {
+    //     float *vector_1 = G.nodes_array[j].vector;
+    //     float *vector_2 = Q.vectors_array[0];
+    //     if (EuclidianDistance(vector_1, vector_2, G.dimension) < 277.0)
+    //     {
+    //         count++;
+    //         std ::cout << "Index : " << j;
+    //         std ::cout << std ::endl;
+    //     }
+    // }
+    // std::cout << "Count : " << count << std ::endl;
+
     return_number = Medoid(&G);
     std::cout << return_number;
 
-    // std::cout << "Now Printing Results from Greedy Function " << std::endl;
-    // result_greedy *GreedyFunction = NULL;
-    // GreedyFunction = Greedy_Search(&G, 15, 20, 30);
-    // std::cout << "Visited Nodes : ";
-    // for (auto &V_element : GreedyFunction->V)
-    //     std::cout << V_element << " ";
-    // std::cout << std::endl;
-    // std::cout << "Visited Nodes Size : " << GreedyFunction->V.size() << std::endl;
-    // std::cout << "L Set : ";
-    // for (auto &L_element : GreedyFunction->L)
-    //     std::cout << L_element << " ";
-    // std ::cout << std::endl;
-    // std::cout << "L Set Size : " << GreedyFunction->L.size() << std::endl;
-    // std::cout << "Printing number of edges before : " << G.nodes_array[15].edges.size();
-    // std::cout << std::endl;
-    // Robust_Prune(15, &(GreedyFunction->V), 1.1, &G);
-    // std::cout << "Printing number of edges after Robust Prune : " << G.nodes_array[15].edges.size();
-    // std::cout << std::endl;
+    std::cout << "Now Printing Results from Greedy Function " << std::endl;
+    result_greedy *GreedyFunction = NULL;
+    GreedyFunction = Greedy_Search(&G, 15, 20, 30);
+    std::cout << "Visited Nodes : ";
+    for (auto &V_element : GreedyFunction->V)
+        std::cout << V_element << " ";
+    std::cout << std::endl;
+    std::cout << "Visited Nodes Size : " << GreedyFunction->V.size() << std::endl;
+    std::cout << "L Set : ";
+    for (auto &L_element : GreedyFunction->L)
+        std::cout << L_element << " ";
+    std ::cout << std::endl;
+    std::cout << "L Set Size : " << GreedyFunction->L.size() << std::endl;
+    std::cout << "Printing number of edges before : " << G.nodes_array[15].edges.size();
+    std::cout << std::endl;
+    Robust_Prune(15, &(GreedyFunction->V), 1.1, &G);
+    std::cout << "Printing number of edges after Robust Prune : " << G.nodes_array[15].edges.size();
+    std::cout << std::endl;
 
-    // delete GreedyFunction;
+    delete GreedyFunction;
 
     Delete_Graph(&G);
 
