5210a372bb27355f1ac5d95c1bd1b589d6b6c8a0
sdi2100098 <sdi2100098@di.uoa.gr>
2024-11-11 18:52:26 +0200

    I give up

--- source_files/Read_Vector.cpp
+++ source_files/Read_Vector.cpp
@@ -206,32 +206,7 @@ fread_error:
     return 1;
 }
 
-// std::vector<std::vector<int>> ReadFileTXT(const char *filename)
-// {
-//     FILE *file = fopen(filename, "r");
-//     if (file == NULL)
-//     {
-//         perror("Error opening File");
-//         return {};
-//     }
 
-//     char line[256];
-//     std::vector<std::vector<int>> info;
-
-//     while (fgets(line, sizeof(line), file))
-//     {
-//         std::vector<int> row;
-
-//         std::istringstream iss(line);
-//         int number;
-//         while (iss >> number)
-//             row.push_back(number);
-
-//         info.push_back(row);
-//     }
-//     fclose(file);
-//     return info;
-// }
 
 int Init_Ground_Truth_Data(const char *file_path, groundTruth *GT)
 {
