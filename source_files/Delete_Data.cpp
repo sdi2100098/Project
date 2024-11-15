ea2fb521f98cd3448655cbaafca8bcd87d21f8ed
sdi2100028 <sdi2100028@di.uoa.gr>
2024-11-15 17:16:34 +0200

    Fix Query

--- source_files/Delete_Data.cpp
+++ source_files/Delete_Data.cpp
@@ -8,11 +8,11 @@ void Delete_Graph(Graph *G){
 
     printf("Dealocate memmory for Graph!\n");
 
-    for(int i = 0; i < G->number_of_nodes; i++){
-        free(G->nodes_array[i].vector);
+    for(int i = 0; i < G->number_of_indexes; i++){
+        free(G->index_array[i].vector);
     }
 
-    delete[] G->nodes_array;
+    delete[] G->index_array;
     delete[] G->Filters;
 }
 
@@ -20,9 +20,10 @@ void Delete_Query(Query *Q){
 
     printf("Dealocate memmory for Query!\n");
 
-    for(int i = 0; i < Q->number_of_nodes; i++){
-        free(Q->nodes_array[i].vector);
+    for(int i = 0; i < Q->number_of_indexes; i++){
+        free(Q->index_array[i].vector);
     }
 
-    free(Q->nodes_array);
+    free(Q->index_array);
+    delete [] Q->Filters;
 }
