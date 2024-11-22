fb7daf14851a9e8be6682999c8bdc4a980ecf7e4
GianKouvelas <gianniskouv13@gmail.com>
2024-11-22 16:01:49 +0200

    dark soul

---
+++ source_files/StitchedVamanaIndexing.cpp
@@ -0,0 +1,18 @@
+#include "fun.hpp"
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+
+int* StichedVamanaIndexing(const char *file_path,const char *filter_file_path, Graph *G, int L_small, int R_small,int R_stitched, double a) {
+    int *s = NULL;
+    Result_greedy *result_greedy = NULL;
+    /*Initialize G to an Empty Graph*/
+    int result = Init_Graph_Data(G, file_path);
+    if (result == 1)
+    {
+        perror("Error in Init_Graph");
+        return NULL;
+    }
+
+    
+}
