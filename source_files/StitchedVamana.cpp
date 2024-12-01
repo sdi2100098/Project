97d5e5786e2df8d2af21e8a38aa2ac9047464a6c
GianKouvelas <gianniskouv13@gmail.com>
2024-12-01 23:34:55 +0200

    newborne2

--- source_files/StitchedVamana.cpp
+++ source_files/StitchedVamana.cpp
@@ -3,11 +3,30 @@
 #include <stdlib.h>
 #include <iostream>
 
+int GetRandomNumber(const int max, const int exclude)
+{
+    int number;
+    do
+    {
+        number = rand() % max;
+    } while (number == exclude); // if number is the same as exclude repeat
+    return number;
+}
+
 int* StichedVamana(const char *file_path, Graph *G, int L_small, int R_small,int R_stitched, double a) {
     int *s = NULL;
     Result_greedy *result_greedy = NULL;
     /*Initialize G to an Empty Graph*/
     int result = Init_Graph_Data(G, file_path, true);
+    for(int i = 0; i < G->number_of_indexes; i++) {
+        G->index_array[i].edges = {};
+        while(G->index_array[i].edges.size() < (size_t)R_small) {
+            GetRandomNumber();
+        } 
+    }
+    G->R = R_small;
+
+
     if (result == 1)
     {
         perror("Error in Init_Graph");
@@ -16,14 +35,10 @@ int* StichedVamana(const char *file_path, Graph *G, int L_small, int R_small,int
     // Create a stitched graph over points
     for (int f = 0; f < G->Filters_Size; f++) {
         std::vector<int> &Pf = G->Filters[f]; // Set of points with label f
-        Graph Gf; // Temporary subgraph for the current filter
-
-        Init_Graph_Data(&Gf, file_path, true); // Initialize Gf for the subset
-        Gf.Filters_Size = 1; // Only one filter active in this subgraph
-        Gf.R = R_small;
+        G->number_of_indexes = Pf.size();
 
         // Call Vamana on the subset of points
-        Vamana(file_path, &Gf, L_small, R_small, a, G->Filters[f]);
+        Vamana(file_path, G, L_small, R_small, a, f);
 
         // Merge edges into the main graph
         for (int i = 0; i < Pf.size(); i++) {
